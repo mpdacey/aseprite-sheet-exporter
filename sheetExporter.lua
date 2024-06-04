4dff57d80d06a0631298e20c55beb2a9ea37fcc1
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-06-04 14:54:37 +1000

    Added constraints UI

--- sheetExporter.lua
+++ sheetExporter.lua
@@ -130,18 +130,74 @@ dlg:combobox{
     onchange=function ()
         if dlg.data.sheetType == "Horizontal Strip" then
             sheetTypeValue = SpriteSheetType.HORIZONTAL
+            dlg:modify{id="constraints", options={"None"}}
         elseif dlg.data.sheetType == "Vertical Strip" then
             sheetTypeValue = SpriteSheetType.VERTICAL
+            dlg:modify{id="constraints", options={"None"}}
         elseif dlg.data.sheetType == "By Rows" then
             sheetTypeValue = SpriteSheetType.ROWS
+            dlg:modify{id="constraints", options={"None", "Fixed # of Columns", "Fixed Width"}}
         elseif dlg.data.sheetType == "By Columns" then
             sheetTypeValue = SpriteSheetType.COLUMNS
+            dlg:modify{id="constraints", options={"None", "Fixed # of Rows", "Fixed Height"}}
         elseif dlg.data.sheetType == "Packed" then
             sheetTypeValue = SpriteSheetType.PACKED
+            dlg:modify{id="constraints", options={"None", "Fixed Width", "Fixed Height", "Fixed Size"}}
         end
     end
 }
 
+dlg:combobox{
+    id="constraints",
+    label="Constraints: ",
+    hexpand=true,
+    option="None",
+    options={"None"},
+    onchange=function ()
+        dlg:modify{id="sizeX", visible=false}
+        dlg:modify{id="sizeY", visible=false}
+        dlg:modify{id="rowCount", visible=false}
+        dlg:modify{id="columnCount", visible=false}
+
+        if dlg.data.constraints == "None" then
+            return
+        end
+
+        if dlg.data.constraints == "Fixed # of Rows" then
+            dlg:modify{id="rowCount", visible=true}
+        elseif dlg.data.constraints == "Fixed # of Columns" then
+            dlg:modify{id="columnCount", visible=true}
+        elseif dlg.data.constraints == "Fixed Width" then
+            dlg:modify{id="sizeX", visible=true}
+        elseif dlg.data.constraints == "Fixed Height" then
+            dlg:modify{id="sizeY", visible=true}
+        elseif dlg.data.constraints == "Fixed Size" then
+            dlg:modify{id="sizeX", visible=true}
+            dlg:modify{id="sizeY", visible=true}
+        end
+    end
+}
+dlg:number {
+    id="sizeX",
+    decimals=integer,
+    visible=false
+}
+dlg:number {
+    id="sizeY",
+    decimals=integer,
+    visible=false
+}
+dlg:number {
+    id="rowCount",
+    decimals=integer,
+    visible=false
+}
+dlg:number {
+    id="columnCount",
+    decimals=integer,
+    visible=false
+}
+dlg:newrow()
 dlg:number {
     id="scaleId",
     label="Scale: ",
